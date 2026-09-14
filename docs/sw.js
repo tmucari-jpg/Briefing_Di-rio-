35b6b755d71112d5023691cb7b1ad5212978d773
tmucari-jpg <tmucari@gmail.com>
2026-09-14 23:50:37 +0200

    fix: atualizar cache da PWA para nova versao visual

--- docs/sw.js
+++ docs/sw.js
@@ -1,5 +1,5 @@
-const CACHE='briefing-v2';
-const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.json'];
+const CACHE='briefing-v3';
+const ASSETS=['./','./index.html','./style.css?v=3','./app.js','./manifest.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
