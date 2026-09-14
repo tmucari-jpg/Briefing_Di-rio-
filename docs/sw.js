bf46e348c20b7b98f9b7ca1f9d9f95ae32715c55
tmucari-jpg <tmucari@gmail.com>
2026-09-15 00:24:09 +0200

    fix: atualizar cache PWA

--- docs/sw.js
+++ docs/sw.js
@@ -1,5 +1,5 @@
-const CACHE='briefing-v3';
-const ASSETS=['./','./index.html','./style.css?v=3','./app.js','./manifest.json'];
+const CACHE='briefing-v4';
+const ASSETS=['./','./index.html','./style.css?v=3','./app.js','./manifest.json?v=2','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
