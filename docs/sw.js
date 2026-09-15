8dc4a46178a09fcf289a6e467e0dca5bdfffa5ef
tmucari-jpg <tmucari@gmail.com>
2026-09-15 15:54:55 +0200

    fix: actualizar cache do PWA para carregar noticias

--- docs/sw.js
+++ docs/sw.js
@@ -1,5 +1,5 @@
-const CACHE='briefing-v6';
-const ASSETS=['./','./index.html','./style.css?v=4','./app.js?v=5','./manifest.json?v=2','./icon.svg'];
+const CACHE='briefing-v7';
+const ASSETS=['./','./index.html','./style.css?v=4','./app.js?v=6','./manifest.json?v=2','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
